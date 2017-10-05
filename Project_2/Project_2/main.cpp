e4b262ec122f8aae4546bc6908cbb177d87435f7
Benjamin Wicker <bwicker@CedarvilleUniversity394.onmicrosoft.com>
2017-10-04 20:55:04 -0400

    File parse working

--- Project_2/Project_2/main.cpp
+++ Project_2/Project_2/main.cpp
@@ -1,10 +1,68 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
+void print(vector<int> v);
+
 int main() {
 
-	cout << "hello world" << endl;
+	// open input file
+	ifstream in;
+	in.open("input.txt");
 
+	// open output file
+	ofstream out;
+	out.open("output.txt");
+
+	// parse file ----------------------------------------
+
+	int n;			// number of denominations
+	int k;			// number of problems to solve
+	vector<int> d;	// vector of denominations
+	vector<int> p;	// vector of problems to solve
+
+	// parse denominations
+	in >> n;
+	d.resize(n);
+	for (int i = 0; i < n; i++) {
+		in >> d[i];
+	}
+
+	// parse problems
+	in >> k;
+	p.resize(k);
+	for (int i = 0; i < k; i++) {
+		in >> p[i];
+	}
+
+	// ---------------------------------------------------
+
+	
+
+
+
+	// test
+	
+	// end test
+
+
+
+
+
+
+
+	// close files
+	in.close();
+	out.close();
 	return 0;
+}
+
+void print(vector<int> v)
+{
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+
 }
--- Project_2/Project_2/parseFile.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-// header for parseFile
-
-#include <iostream>
-using namespace std;
-
-void parseFile(int& n, int* d, int& k, int* v, string filename);
